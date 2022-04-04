b9158db0b2f6641e2545408b41689a88001a214b
mizokichicode <mizokichicode@gmail.com>
2022-04-04 21:52:56 +0900

    add: module

--- pysample/pysample.py
+++ pysample/pysample.py
@@ -1,10 +1,14 @@
 if __package__ is None:
     import calc
+    import module
 else:
     from . import calc
+    from . import module
 
 
 def main():
+    module.hello()
+    print()
     print('add           : ', calc.add(10, 8))
     print('subtraction   : ', calc.subtraction(10, 8))
     print('multiplication: ', calc.multiplication(10, 8))
